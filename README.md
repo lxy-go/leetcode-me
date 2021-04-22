a421bb23b0f7ce1f69a0ec1c71571bb7002d3110
lixiyan <lixiyan@zhuanzhuan.com>
2021-04-22 15:12:02 +0800

    add leetcode: 70

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is leetcode practice
 - Solution19: [删除倒数第K个位置](https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list)
 - Solution21: [两个有序链表合并](https://leetcode-cn.com/problems/merge-two-sorted-lists/description/)
 - Solution24: [交换链表互相的值](https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/)
+- Solution70: [爬楼梯](https://leetcode-cn.com/problems/climbing-stairs/description/)
 - Solution83: [删除排序链表重复元素](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list)
 ## 101-200
 - Solution160: [链表交点](https://leetcode-cn.com/problems/intersection-of-two-linked-lists/description/)
---
+++ base/src/com/leetcode/algorithms/solution100/Solution70.java
@@ -0,0 +1,52 @@
+package com.leetcode.algorithms.solution100;
+
+/**
+ * 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ * @author lixiyan
+ * @date 2021/4/22 3:01 PM
+ */
+public class Solution70 {
+
+    /**
+     * 动态规划，从上到下
+     *
+     * 状态转移公式 dp[i] = dp[i-1]+dp[i-2];
+     */
+    private static int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    /**
+     * 优化空间
+     * p1:上一步 p2下一步，tmp当前步
+     */
+    private static int climbStairs2(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int p1 = 1,p2 =2;
+        for (int i = 2; i <= n; i++) {
+            int tmp = p2+p1;
+            p1 = p2;
+            p2 = tmp;
+        }
+        return p1;
+    }
+
+    public static void main(String[] args) {
+        int i = climbStairs2(4);
+        System.out.println(i);
+    }
+}
