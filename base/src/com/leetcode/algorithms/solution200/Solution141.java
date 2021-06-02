a6f77eb5b70915d9ef3e2c0fb31db6f78141d1e0
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-02 20:09:41 +0800

    add leetcode: 141

--- base/src/com/leetcode/algorithms/solution200/Solution141.java
+++ base/src/com/leetcode/algorithms/solution200/Solution141.java
@@ -14,19 +14,20 @@ public class Solution141 {
      * 链表，双指针
      */
     public static  boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return false;
-        }
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if (slow == fast){
-                return true;
-            }
-        }
-        return false;
+       if(head == null || head.next == null){
+           return false;
+       }
+       ListNode slow = head;
+       ListNode fast = head.next;
+       while(fast != null && fast.next != null){
+           if(slow == fast){
+               return true;
+           }else {
+               slow = slow.next;
+               fast = fast.next.next;
+           }
+       }
+       return false;
     }
 
     public static void main(String[] args) {
