f5845180cb9a6c7009a32f8d54a6141bc03e0bf1
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-02 15:02:17 +0800

    add leetcode: 167

--- base/src/com/leetcode/algorithms/solution200/Solution167.java
+++ base/src/com/leetcode/algorithms/solution200/Solution167.java
@@ -12,19 +12,22 @@ public class Solution167 {
      * 双指针
      */
     private static int[] twoSum(int[] numbers, int target) {
-        int i =0;
-        int j=numbers.length-1;
+        if (numbers == null){
+            return null;
+        }
+        int i =0,j=numbers.length-1;
         while(i < j){
-          int sum= numbers[i] +numbers[j];
-          if (sum == target){
-              return new int[]{i+1,j+1};
-          }else if(sum > target){
-              j--;
-          }else {
-              i++;
-          }
+            int sum = numbers[i]+numbers[j];
+            if (sum == target){
+                return new int[]{i,j};
+            }else if(sum > target){
+                j--;
+            }else {
+                i++;
+            }
         }
         return null;
+
     }
 
     public static void main(String[] args) {
