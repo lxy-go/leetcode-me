df5d137bc6075327705a2990e53f68ae144aaa84
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-02 16:33:45 +0800

    add leetcode: 680

--- base/src/com/leetcode/algorithms/solution700/Solution680.java
+++ base/src/com/leetcode/algorithms/solution700/Solution680.java
@@ -13,25 +13,25 @@ public class Solution680 {
      * 回文字符串
      */
     public static boolean validPalindrome(String s) {
-        for (int i = 0, j =s.length()-1; i<j;i++,j--) {
-            if (s.charAt(i) != s.charAt(j)){
-                return isPalindrome(s,i-1,j) || isPalindrome(s,i,j-1);
+        for(int i=0,j = s.length()-1;i<j;i++,j--){
+            if(s.charAt(i) != s.charAt(j) ){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
             }
         }
         return true;
     }
 
     private static boolean isPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i++) != s.charAt(j--)) {
-                return false;
-            }
-        }
-        return true;
+       while(i < j){
+           if(s.charAt(i++) != s.charAt(j--) ){
+               return false;
+           }
+       }
+       return true;
     }
 
     public static void main(String[] args) {
-        String s = "abcba";
+        String s = "abc";
         System.out.println(validPalindrome(s));
     }
 }
