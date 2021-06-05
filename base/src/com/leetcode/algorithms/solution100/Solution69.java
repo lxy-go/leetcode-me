81f461e144356ec4680bf12fa9b7af3e667f3234
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-05 18:09:41 +0800

    add leetcode: 69

--- base/src/com/leetcode/algorithms/solution100/Solution69.java
+++ base/src/com/leetcode/algorithms/solution100/Solution69.java
@@ -8,24 +8,30 @@ package com.leetcode.algorithms.solution100;
  */
 public class Solution69 {
 
-    public static int mySqrt(int x) {
-        int l=1,h=x;
-        while(l <= h){
-            int mid = l+ (h-l)/2;
-            int sqrt = x/mid;
-            if (mid == sqrt){
-                return mid;
-            }else if(mid > sqrt){
-                h = mid-1;
-            }else {
-                l=mid+1;
-            }
-        }
-        return h;
+    /**
+     * 二分法
+     */
+    private static int mySqrt(int x) {
+        if (x<=1){return x;}
+       int l = 0;
+       int h = x;
+       while(l<=h){
+           int mid = l+(h-l)/2;
+           int sqrt = x/mid;
+           // 开方数更大
+           if(sqrt > mid){
+               l = mid+1;
+           }else if (sqrt<mid){
+               h = mid-1;
+           }else {
+               return sqrt;
+           }
+       }
+       return h;
     }
 
     public static void main(String[] args) {
-        int a = mySqrt(8);
+        int a = mySqrt(1);
         System.out.println(a);
     }
 }
