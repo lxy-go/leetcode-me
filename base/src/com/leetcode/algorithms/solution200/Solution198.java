16dbf2af73f05d6d0f8a001c57aa8a195a132ccb
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-06 18:19:47 +0800

    add leetcode: 198

--- base/src/com/leetcode/algorithms/solution200/Solution198.java
+++ base/src/com/leetcode/algorithms/solution200/Solution198.java
@@ -13,22 +13,15 @@ public class Solution198 {
      * 状态转移方程：dp[i] = Max(dp[i-2]+num[i],dp[i-1])
      */
     private static int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if(nums == null || nums.length == 0){
             return 0;
         }
-        int length = nums.length;
-        if (length == 1) {
-            return nums[0];
+        int[] dp = new int[nums.length+1];
+        dp[1]= nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(nums[i-1]+dp[i-2],dp[i-1]);
         }
-        int[] dp = new int[length];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
-        }
-        return dp[length - 1];
+        return dp[nums.length];
     }
 
     /**
@@ -48,7 +41,7 @@ public class Solution198 {
 
     public static void main(String[] args) {
         int[] arr = {2, 7, 9, 3, 1};
-        int rob = rob2(arr);
+        int rob = rob(arr);
         System.out.println(rob);
 
     }
