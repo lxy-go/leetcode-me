f3d7264c0ddfc90fb0a68a6f6ef571ddf34aa741
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-11 16:08:46 +0800

    add leetcode: byte014

--- base/src/com/leetcode/interview/bytedancer/Solution014.java
+++ base/src/com/leetcode/interview/bytedancer/Solution014.java
@@ -11,10 +11,10 @@ public class Solution014 {
     /**
      * 二分法
      */
-    public int kthSmallest(int[][] matrix, int k) {
+    public static int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
         int left = matrix[0][0];
-        int right = matrix[n-1][n+1];
+        int right = matrix[n-1][n-1];
         while(left < right){
             int mid = left + (right-left)/2;
             if (check(matrix,mid,k,n)){
@@ -31,6 +31,7 @@ public class Solution014 {
         int num = 0;
         while(i >= 0 && j < n){
             if (matrix[i][j] <= mid){
+
                 num += i+1;
                 j++;
             }else {
@@ -39,5 +40,10 @@ public class Solution014 {
 
         }
         return num>=k;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,5,9},{10,11,13},{12,13,15}};
+        System.out.println(kthSmallest(matrix, 8));
     }
 }
