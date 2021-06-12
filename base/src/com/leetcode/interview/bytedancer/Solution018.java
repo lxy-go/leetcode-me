5dc1e844c4f6ead12d50f7f57283fda2aabdce9c
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-12 16:50:55 +0800

    add leetcode: byte018

---
+++ base/src/com/leetcode/interview/bytedancer/Solution018.java
@@ -0,0 +1,35 @@
+package com.leetcode.interview.bytedancer;
+
+/**
+ * 有序找目标值
+ *
+ * @author lixiyan
+ * @date 2021/6/11 5:30 PM
+ */
+public class Solution018 {
+
+    /**
+     * 二分法
+     */
+    private static int find(int[] nums,int target){
+        int l = 0;
+        int h = nums.length;
+        while(l<h){
+            int mid = l +(h-l)/2;
+            if (nums[mid] == target){
+                return mid;
+            }else if(nums[mid]>target){
+                h = mid -1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6};
+        int target = 7;
+        System.out.println(find(nums, target));
+    }
+}
