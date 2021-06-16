0cdf762e6d35f6b1025348e30e2d156537251a83
lixiyan <lixiyan@zhuanzhuan.com>
2021-06-16 12:55:17 +0800

    add leetcode: byte008

--- base/src/com/leetcode/interview/re/Solution008.java
+++ base/src/com/leetcode/interview/re/Solution008.java
@@ -1,10 +1,27 @@
 package com.leetcode.interview.re;
 
+import com.leetcode.base.TreeNode;
+
 /**
- * TODO
+ * 根节点到叶子节点为N路径
  *
  * @author lixiyan
  * @date 2021/6/16 12:48 PM
  */
 public class Solution008 {
+
+    private static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null){
+            return false;
+        }
+        if (root.left == null && root.right==null){
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
