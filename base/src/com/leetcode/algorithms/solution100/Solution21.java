fa6d4e34622103fb25e5bca531bf607a13df263d
lixiyan <lixiyan@zhuanzhuan.com>
2021-07-31 17:44:47 +0800

    add leetcode: 21

--- base/src/com/leetcode/algorithms/solution100/Solution21.java
+++ base/src/com/leetcode/algorithms/solution100/Solution21.java
@@ -37,13 +37,13 @@ public class Solution21 {
      * 合并两个有序链表(递归)
      */
     private static Node mergeTwoList2(Node l1,Node l2){
-        if (l1 == null){return l2;}
+        if(l1 == null){return l2;}
         if (l2 == null){return l1;}
         if (l1.val < l2.val){
             l1.next = mergeTwoList(l1.next,l2);
             return l1;
         }else {
-            l2.next = mergeTwoList(l1,l2.next);
+            l2.next = mergeTwoList(l1, l2.next);
             return l2;
         }
 
